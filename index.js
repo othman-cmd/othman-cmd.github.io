c4af22c20d4250073a7cfb50bfd6757542e968c2
othman-cmd <othman_690@hotmail.com>
2021-01-07 12:34:30 +0100

    add backround

---
+++ index.js
@@ -0,0 +1,9 @@
+// console.log("hello")
+// var age = prompt("how old are you?")
+// var allowed = 18
+// if (age > allowed) {
+//     alert("Welcome")
+// }
+document.getElementById("klickbutton").addEventListener("click", function () {
+    console.log("Button was clicked")
+})
